1c77945b917b46b76af843d7d1755dc9510f8ace
IsabelMarleen <isabel-marleen@t-online.de>
2023-03-03 15:13:49 +0100

    Include setting up demes spec in tests

--- tests/testthat/test-read_demes.R
+++ tests/testthat/test-read_demes.R
@@ -1,6 +1,7 @@
 # TODO: this should be more extensive (perhaps extract this into a function and run
 # against multiple (or even all?) YAML files in the Demes spec repository)
 test_that("minimal_01.yaml is loaded correctly", {
+  setup_demes_spec()
   d <- read_demes(file = file.path(get_spec_dir(), "test-cases", "valid", "minimal_01.yaml"))
 
   # Test that all the expected list entries are present in the loaded
@@ -20,15 +21,16 @@ test_that("minimal_01.yaml is loaded correctly", {
   expect_equal(d$metadata, named_list)
   expect_equal(d$demes[[1]]$ancestors, list())
   expect_identical(d$demes[[1]]$description, '')
-  expect_equal(d$demes[[1]]$proportions, vector(mode="integer"))
+  expect_equal(d$demes[[1]]$proportions, list())
   expect_identical(d$migrations, list())
   expect_identical(d$pulses, list())
 })
 
 test_that("both input types work", {
+  setup_demes_spec()
   yaml_string <- "time_units: generations\ndemes:\n  - name: a\n    epochs:\n    - start_size: 100"
   a <- read_demes(text = yaml_string)
-  b <- read_demes(file = get_test_file("minimal_01.yaml"))
+  b <- read_demes(file = file.path(get_spec_dir(), "test-cases", "valid", "minimal_01.yaml"))
   expect_identical(order_demes(a), order_demes(b))
 })
 
